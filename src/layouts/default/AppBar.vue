b6deb6bf3117f98b7a766e639327197682e85131
Di3goCLima <diegocliimaa@gmail.com>
2023-06-21 00:10:04 -0300

    new file:   .browserslistrc 	new file:   .editorconfig 	new file:   .eslintrc.js 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   jsconfig.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/AGE_logo.png 	new file:   src/assets/logo.svg 	new file:   src/components/HelloWorld.vue 	new file:   src/layouts/default/AppBar.vue 	new file:   src/layouts/default/Default.vue 	new file:   src/layouts/default/View.vue 	new file:   src/main.js 	new file:   src/plugins/index.js 	new file:   src/plugins/vuetify.js 	new file:   src/plugins/webfontloader.js 	new file:   src/router/index.js 	new file:   src/store/app.js 	new file:   src/store/index.js 	new file:   src/styles/settings.scss 	new file:   src/views/Home.vue 	new file:   vite.config.js

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar color="indigo-darken-4">
+    <v-img class="mx-2" src="/src/assets/AGE_logo.png" max-height="120" max-width="120">
+    </v-img>
+  </v-app-bar>
+</template>
+
+<script setup>
+  //
+</script>
+
